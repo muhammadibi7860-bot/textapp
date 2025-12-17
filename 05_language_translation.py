6b0c9c036a86a3822684088e029f5f45b35b2126
muhammadibi7860 <muhammadibi7860@gmail.com>
2025-12-17 22:31:06 +0500

    Revert "Dynamic language mapping"
    This reverts commit f8d820e0a40a7e4a54cd4dcfc0b10528d9bceb62.

--- 05_language_translation.py
+++ 05_language_translation.py
@@ -1,5 +1,4 @@
 from googletrans import Translator
-from language_mapping import language_codes
 
 # Sample text - "Hello, how are you doing?"
 
@@ -10,18 +9,9 @@ if __name__ == "__main__":
         if input_text.lower() == 'exit':
             print("Exiting...")
             break
-        
-        while True:
-            dest_language_name = input("Enter the destination language (e.g., 'French'): ").capitalize()
-
-            try:
-                dest_language_code = language_codes[dest_language_name]
-                break  # Break out of the inner loop if a valid language code is found
-            except KeyError:
-                print("Invalid language name. Please try again.")
 
         translator = Translator()
-        translated_text = translator.translate(input_text, dest=dest_language_code).text
+        translated_text = translator.translate(input_text, dest='fr').text
 
         print(f"Original Text: {input_text}")
         print(f"Translated Text: {translated_text}")
